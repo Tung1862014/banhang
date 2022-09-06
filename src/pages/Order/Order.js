7679b6454fd5be65c6e66fb98c401cc61cba4bda
Tung1862014 <Mayuri1862014@gmail.com>
2022-09-06 21:51:19 +0700

    30 buiding order #2 (api)

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,11 +1,122 @@
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import classNames from 'classnames/bind';
+import { useEffect, useState } from 'react';
+import GetCookie from '~/components/Hook/GetCookies';
 import styles from './Order.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Order() {
+    const [sellerValue, setSellerValue] = useState('');
+    const [sellerName, setSellerName] = useState('');
+    const [orderValue, setOrderValue] = useState('');
+    const [sumNumber, setSumNumber] = useState('');
+    const [price, setPrice] = useState('');
+
+    useEffect(() => {
+        axios
+            .get(
+                `${process.env.REACT_APP_URL_NODEJS}/orderproduct/user/show?ND_id=${
+                    JSON.parse(GetCookie('usrin')).ND_id
+                }`,
+            )
+            .then((res) => {
+                console.log('data', res.data);
+            })
+            .catch((err) => {
+                console.log('loi');
+            });
+    }, []);
+
+    useEffect(() => {
+        axios
+            .get(
+                `${process.env.REACT_APP_URL_NODEJS}/cartcustomer/cart/show/all?ND_id=${
+                    JSON.parse(GetCookie('usrin')).ND_id
+                }`,
+            )
+            .then((res) => {
+                //console.log(res.data);
+                setOrderValue(res.data.results);
+            })
+            .catch((err) => {
+                console.log('loi');
+            });
+    }, []);
+
+    useEffect(() => {
+        let sellerArr = [];
+        let sellerName = [];
+        let sumnumber = [];
+        let price = [];
+
+        for (let i = 0; i < orderValue.length; i++) {
+            if (!sellerArr.includes(orderValue[i].NB_id)) {
+                sellerArr.push(orderValue[i].NB_id);
+                sellerName.push(orderValue[i].seller.NB_hoten);
+            }
+
+            sumnumber += 1;
+            price +=
+                orderValue[i].product.SP_gia *
+                orderValue[i].TTDH_soluong *
+                ((100 - orderValue[i].product.SP_khuyenmai) / 100);
+        }
+        console.log('sellerArr', sellerArr);
+        if (sellerArr.length > 0) {
+            setSellerValue((prev) => {
+                const newSeller = [...prev, sellerArr];
+                return newSeller[0];
+            });
+            setSellerName((prev) => {
+                const newSeller = [...prev, sellerName];
+                return newSeller[0];
+            });
+        }
+
+        setSumNumber(sumnumber);
+        setPrice(price);
+    }, [orderValue]);
+
+    function formatCash(str) {
+        return str
+            .toString()
+            .split('')
+            .reverse()
+            .reduce((prev, next, index) => {
+                return (index % 3 ? next : next + '.') + prev;
+            });
+    }
+
+    const handleSumProduct = (sell) => {
+        let numbers = 0;
+        console.log('sell', sell);
+        for (let i = 0; i < orderValue.length; i++) {
+            if (sell === orderValue[i].NB_id) {
+                numbers += 1;
+            }
+        }
+        console.log('index', numbers);
+        return numbers;
+    };
+
+    const handlePriceSeller = (sell) => {
+        let price = 0;
+        console.log('sell', sell);
+        for (let i = 0; i < orderValue.length; i++) {
+            if (sell === orderValue[i].NB_id) {
+                price +=
+                    orderValue[i].product.SP_gia *
+                    orderValue[i].TTDH_soluong *
+                    ((100 - orderValue[i].product.SP_khuyenmai) / 100);
+            }
+        }
+        console.log('index', price);
+        return price;
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('ZrE0do')}>
@@ -21,46 +132,78 @@ function Order() {
                     </div>
                 </div>
                 {/* / */}
-                <div>
-                    <div className={cx('dwwHJ-')}>
-                        <div>
-                            <div className={cx('nF6yNn')}>
-                                <div className={cx('yknSi4')}>
-                                    <div className={cx('WJIXj0')}>haisanmekong</div>
-                                </div>
-                                <div className={cx('CZ6uu2')}>
-                                    <div className={cx('_6kMvNg_ka6CzP')}>
-                                        <div className={cx('_4MGXB1_c7N4lb')}>
-                                            <img
-                                                className={cx('GCGEKm')}
-                                                alt=""
-                                                src="https://cf.shopee.vn/file/2b2db8d56d4ccddc3c0645cdb9507594_tn"
-                                                width="40"
-                                                height="40"
-                                            />
-                                            <span className={cx('F8X-cZ')}>
-                                                <span className={cx('tPzkNt')}>
-                                                    Khô cá lóc ,hàng loại đặc biệt đủ 3 nắng Đồng Tháp ,thịt dai,ngọt
-                                                    ,mặn vừa ăn ,sản phẩm trọng lượng đúng chuẩn
-                                                </span>
-                                            </span>
-                                        </div>
-                                        <div className={cx('_4MGXB1')}>₫119.000</div>
-                                        <div className={cx('_4MGXB1')}>1</div>
-                                        <div className={cx('_4MGXB1_8fgmps')}>₫119.000</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* / */}
-                        <div className={cx('BbOmi')}>
-                            <div className={cx('lYtB1r')}>
-                                <div className={cx('_4nelpz')}>Tổng số tiền (1 sản phẩm):</div>
-                                <div className={cx('_31ayp3')}>₫151.800</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {sellerValue !== ''
+                    ? sellerValue.map((sell, index) => (
+                          <div key={index}>
+                              <div className={cx('dwwHJ-')}>
+                                  <div>
+                                      <div className={cx('nF6yNn')}>
+                                          <div className={cx('yknSi4')}>
+                                              <div className={cx('WJIXj0')}>{sellerName[index]}</div>
+                                          </div>
+                                          {orderValue !== ''
+                                              ? orderValue.map((order, index) =>
+                                                    sell === order.NB_id ? (
+                                                        <div key={index} className={cx('CZ6uu2')}>
+                                                            <div className={cx('_6kMvNg_ka6CzP')}>
+                                                                <div className={cx('_4MGXB1_c7N4lb')}>
+                                                                    <img
+                                                                        className={cx('GCGEKm')}
+                                                                        alt=""
+                                                                        src={
+                                                                            order.product.SP_image ||
+                                                                            process.env.REACT_APP_URL_NODEJS_IMAGE +
+                                                                                '/default-ui-image.webp'
+                                                                        }
+                                                                        width="40"
+                                                                        height="40"
+                                                                    />
+                                                                    <span className={cx('F8X-cZ')}>
+                                                                        <span className={cx('tPzkNt')}>
+                                                                            {order.product.SP_ten}
+                                                                        </span>
+                                                                    </span>
+                                                                </div>
+                                                                <div className={cx('_4MGXB1')}>
+                                                                    ₫
+                                                                    {formatCash(
+                                                                        order.product.SP_gia *
+                                                                            ((100 - order.product.SP_khuyenmai) / 100),
+                                                                    )}
+                                                                </div>
+                                                                <div className={cx('_4MGXB1')}>
+                                                                    {order.TTDH_soluong}
+                                                                </div>
+                                                                <div className={cx('_4MGXB1_8fgmps')}>
+                                                                    ₫
+                                                                    {formatCash(
+                                                                        order.product.SP_gia *
+                                                                            order.TTDH_soluong *
+                                                                            ((100 - order.product.SP_khuyenmai) / 100),
+                                                                    )}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    ) : (
+                                                        ''
+                                                    ),
+                                                )
+                                              : ''}
+                                      </div>
+                                  </div>
+                                  {/* / */}
+                                  <div className={cx('BbOmi')}>
+                                      <div className={cx('lYtB1r')}>
+                                          <div className={cx('_4nelpz')}>
+                                              Tổng số tiền ({handleSumProduct(sell)} sản phẩm):
+                                          </div>
+                                          <div className={cx('_31ayp3')}>₫{formatCash(handlePriceSeller(sell))}</div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      ))
+                    : ''}
             </div>
             <div className={cx('KbJ00X')}>
                 <div className={cx('EQBEfe')}>
@@ -94,7 +237,7 @@ function Order() {
                                 Phương thức thanh toán
                             </div>
                             <div className={cx('checkout-payment-setting__payment-methods-tab')}>
-                                <span tabindex="0">
+                                <span tabIndex="0">
                                     <button
                                         className={cx('product-variation--selected')}
                                         aria-label="Ví ShopeePay"
@@ -116,7 +259,7 @@ function Order() {
                                         </div>
                                     </button>
                                 </span>
-                                <span tabindex="0">
+                                <span tabIndex="0">
                                     <button className={cx('product-variation--selected')}>
                                         Thẻ Tín dụng/Ghi nợ
                                         <div className={cx('product-variation__tick')}>
@@ -134,7 +277,7 @@ function Order() {
                                         </div>
                                     </button>
                                 </span>
-                                <span tabindex="0">
+                                <span tabIndex="0">
                                     <button className={cx('product-variation--selected')}>
                                         Thanh toán khi nhận hàng
                                         <div className={cx('product-variation__tick')}>
@@ -174,7 +317,7 @@ function Order() {
                     <div className={cx('lhwDvd_Exv9ow_A4gPS6')}>Tổng thanh toán:</div>
                     <div className={cx('lhwDvd_0tdvp_Uu2y3K_A4gPS6')}>₫293.300</div>
                     <div className={cx('Ql2fz0')}>
-                        <button class={cx('stardust-button--large_gG-FcK')}>Đặt hàng</button>
+                        <button className={cx('stardust-button--large_gG-FcK')}>Đặt hàng</button>
                     </div>
                 </div>
             </div>
